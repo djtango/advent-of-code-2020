48b76fd3ad41acda7c782741519646125a502607
Deon Tan <deonjtan@gmail.com>
2020-12-06 18:42:50 +0000

    boy am I happy I chose lists instead of tuples

--- day1/hs/src/day1.hs
+++ day1/hs/src/day1.hs
@@ -3,12 +3,13 @@ import Data.Maybe
 main :: IO()
 main =
   getInput "/tmp/aoc1" >>= (\ xs ->
-    putStrLn $ extractValue $ filter isJust $ find2020Product <$> crossProduct xs xs)
+    putStrLn $ extractValue $ filter isJust $ find2020Product <$> tripleProduct xs xs xs)
 
 
 input = [1721,979,366,299,675,1456]
 
 crossProduct left right = sequence [left, right]
+tripleProduct left middle right = sequence [left, middle, right]
 
 find2020Product xs = if (sum xs) == 2020 then Just $ product xs else Nothing
 extractValue = show . fromJust . head
