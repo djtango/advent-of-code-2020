7f30780d42cdc319db5710a8a60be733bc5b1cdb
Deon Tan <deonjtan@gmail.com>
2020-12-06 17:57:13 +0000

    star1 haskell

--- day1/hs/src/day1.hs
+++ day1/hs/src/day1.hs
@@ -1,2 +1,17 @@
+import Data.Maybe
+
 main :: IO()
-main = putStrLn "hello world"
+main =
+  getInput "/tmp/aoc1" >>= (\ xs ->
+    putStrLn $ extractValue $ filter isJust $ find2020Product <$> crossProduct xs xs)
+
+
+input = [1721,979,366,299,675,1456]
+
+crossProduct left right = sequence [left, right]
+
+find2020Product xs = if (sum xs) == 2020 then Just $ product xs else Nothing
+extractValue = show . fromJust . head
+
+getInput :: FilePath -> IO [Int]
+getInput f = readFile f >>= return . lines >>= return . (read <$>)
