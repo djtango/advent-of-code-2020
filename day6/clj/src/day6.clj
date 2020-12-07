ba92b5c73052aa6b29d70a747f0c43b8357e3a70
Deon Tan <deonjtan@gmail.com>
2020-12-07 12:35:31 +0000

    wip - parse rules

--- day6/clj/src/day6.clj
+++ day6/clj/src/day6.clj
@@ -26,7 +26,8 @@
               (catch Throwable t (.printStackTrace t))))
 
 
-(s/def ::bag-stuff (s/alt :bags (s/+ ::amount-of-bags) :end #{"no other bags."}))
+(s/def ::end (s/cat :no #{"no"} :other #{"other"} :bags #{"bags."}))
+(s/def ::bag-stuff (s/alt :bags (s/+ ::amount-of-bags) :end ::end))
 
 (comment (s/exercise ::bag-stuff))
 (s/def ::contain #{"contain"})
@@ -40,7 +41,37 @@
      (s/resolve-spec (cons `s/cat (interleave tag-names spec-names)))))
   (def -rule (cat-of ::colour ::contain ::bag-stuff)))
 
-(s/def ::rule (s/cat :c ::colour :- ::contain :rule ::bag-stuff))
+(s/def ::rule (s/cat :c ::colour :bags ::bags :- ::contain :rule ::bag-stuff))
+(def rules
+  ["light red bags contain 1 bright white bag, 2 muted yellow bags."
+   "dark orange bags contain 3 bright white bags, 4 muted yellow bags."
+   "bright white bags contain 1 shiny gold bag."
+   "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags."
+   "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags."
+   "dark olive bags contain 3 faded blue bags, 4 dotted black bags."
+   "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags."
+   "faded blue bags contain no other bags."
+   "dotted black bags contain no other bags."])
+
+(defn parse [rules]
+  (let [get-rule (comp :rule)
+        ->rule (juxt :c get-rule)
+        normalise (fn [rule]
+                    (-> rule
+                        (update 1 #(if (= :bags (first %))
+                                     (second %)
+                                     []))))]
+    (->> rules
+       (map #(clojure.string/split % #" "))
+       (map #(s/conform ::rule %) )
+       (map ->rule)
+       (map normalise)
+       (into {}))))
+
+(comment
+  (parse rules)
+  )
+
 (comment (clojure.pprint/pprint (s/exercise ::rule)))
 
 (defn -main []
