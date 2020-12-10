31057c129a6f77bf2faf6da18c1d6129c47ec82b
Deon Tan <deonjtan@gmail.com>
2020-12-10 14:30:39 +0000

    day8 part1

--- day8/java/aoc/src/main/java/aoc/Day8.java
+++ day8/java/aoc/src/main/java/aoc/Day8.java
@@ -9,6 +9,41 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Day8 {
+    private static class State{
+        public int acc;
+        public int cmd;
+        public ArrayList<Integer> cmds;
+        public State() {
+            this.acc = 0;
+            this.cmd = 0;
+            this.cmds = new ArrayList();
+        }
+
+        public State setAcc(int n) {
+            this.acc = n;
+            return this;
+        }
+
+        public State setCmd(int n) {
+            this.cmd = n;
+            this.cmds.add(n);
+            return this;
+        }
+
+        public int getAcc() {
+            return this.acc;
+        }
+
+        public ArrayList<Integer> getCmds() {
+            return this.cmds;
+        }
+
+        public int getCmd() {
+            return this.cmd;
+        }
+    }
+
+    public State s;
     private static ArrayList<String> getInput() {
         // String[] ss = {
         //     "nop +0",
@@ -44,11 +79,50 @@ public class Day8 {
         }
     }
 
+    private void jmp(int i) {
+        this.s.setCmd(this.s.getCmd() + i);
+    }
+
+    private void acc(int i) {
+        this.s.setAcc(this.s.getAcc() + i);
+        this.s.setCmd(this.s.getCmd() + 1);
+    }
+
+    private void nop(int i) {
+        this.s.setCmd(this.s.getCmd() + 1);
+    }
+
+    private boolean isCycle() {
+        ArrayList<Integer> cmds = this.s.getCmds();
+        int cmd = this.s.getCmd();
+        System.out.println( cmd );
+        System.out.println( cmds.indexOf(cmd));
+        System.out.println( cmds.lastIndexOf(cmd));
+        return cmds.lastIndexOf(cmd) != (cmds.indexOf(cmd));
+    }
+
     public static void main( String[] args ) {
+        System.out.println("foo");
+        Day8 d = new Day8();
+        d.s = new State();
         ArrayList<String> input = getInput();
-        int i;
-        for ( i = 0; i < input.size(); i +=1 ) {
-            System.out.println( input.get(i) );
+        int i = 0;
+        int cmd = 0;
+        int arg = 0;
+        while (! d.isCycle() && i < 1000) {
+            String[] line = input.get(cmd).split(" ");
+            String instruction = line[0];
+            arg = Integer.parseInt(line[1]);
+            switch (instruction) {
+                case "jmp": d.jmp(arg); break;
+                case "acc": d.acc(arg); break;
+                case "nop": d.nop(arg); break;
+            }
+            System.out.println( cmd );
+            System.out.println( input.get(cmd) );
+            i += 1;
+            cmd = d.s.getCmd();
         }
+        System.out.println(d.s.getAcc());
     }
 }
