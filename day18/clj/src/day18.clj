1679d15d0e5f8241035774853e05d698c2eae1e7
Deon Tan <deonjtan@gmail.com>
2020-12-18 21:24:17 +0000

    part2 working

--- day18/clj/src/day18.clj
+++ day18/clj/src/day18.clj
@@ -15,12 +15,13 @@
         (nth x 2)))
 
 (defn ->sexp [not-sexp]
-  (loop [xs not-sexp]
+  (loop [xs not-sexp cnt 0]
     (let [[e1 op e2 & rst] xs]
-      (if (= 3 (count xs))
+      (if (or (= 3 (count xs)) (> cnt 100))
         (flip xs)
         (recur (concat (list (list op e1 e2))
-                       rst))))))
+                       rst)
+               (inc cnt))))))
 ;; (->sexp '(8 * 3 + 9 + 3 * 4 * 3))
 ;; => '(* (* (+ (+ (* 8 3) 9) 3) 4) 3)
 
@@ -31,6 +32,61 @@
         (->sexp x)
         x))
     line))
+
+(defn prioritise-+ [expr]
+  (loop [xs expr result []]
+    (let [[e1 op e2 & rst] xs]
+      (cond
+        (= 3 (count xs))
+        (seq
+          (cond
+            (empty? result)
+            xs
+
+            (= '* op)
+            (conj result e1 op e2)
+            :else (conj result (list e1 op e2))))
+
+        (= '* op)
+        (recur (cons e2 rst)
+               (conj result e1 op))
+        :else
+        (recur (cons (list e1 op e2) rst)
+               result)))))
+
+;; (prioritise-+ '(8 * 3 + 9 + 3 * 4 * 3))
+;; => '(* (* (* 8 (+ (+ 3 9) 3)) 4) 3)
+;; => '(8 * ((3 + 9) + 3) * 4 * 3)
+;; (prioritise-+ '(1 + (2 * 3) + (4 * (5 + 6))))
+;; '((1 + (2 * 3)) + (4 * (5 + 6)))
+;; (prioritise-+ '(2 * 3 + (4 * 5)))
+
+
+
+(defn order-precedence [line]
+  (walk/postwalk
+    (fn [x]
+      (println x)
+      (cond
+        (and (seq? x) (= 3 (count x)))
+        x
+
+        (seq? x)
+        (prioritise-+ x)
+
+        :else
+        x))
+    line))
+
+(comment
+  (do
+    (assert
+      (= (order-precedence '(1 + (2 * 3) + (4 * (5 + 6))))
+         '((1 + (2 * 3)) + (4 * (5 + 6)))))
+
+    (assert
+      (= (order-precedence '(2 * 3 + (4 * 5)))
+         '(2 * (3 + (4 * 5)))))))
 
 ;; (rearrange '(8 * 3 + 9 + 3 * 4 * 3))
 ;; (rearrange '(5 + (8 * 3 + 9 + 3 * 4 * 3)))
@@ -46,3 +102,23 @@
        (map rearrange)
        (map eval)
        (reduce +)))
+
+(defn part2 [i]
+  (->> (get-input)
+       parse-input
+       (map order-precedence)
+       (map rearrange)
+       (map eval)
+       (reduce +)))
+
+;; (prioritise-+ '((1 + (2 * 3)) + (4 * (5 + 6))))
+;; => (eval (rearrange (order-precedence '(((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2))))
+;;=> (rearrange '((((((2 + 4) * 9) * (((6 + 9) * (8 + 6)) + 6)) + 2) + 4) * 2))
+
+
+(comment
+  (part2 ['(1 + (2 * 3) + (4 * (5 + 6)))
+          '(2 * 3 + (4 * 5))
+          '(5 + (8 * 3 + 9 + 3 * 4 * 3))
+          '(5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4)))
+          '(((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2)]))
